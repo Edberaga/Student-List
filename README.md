472e181870926ec4d5b927e7cb617f9ed78284e8
Edberaga <72023119+Edberaga@users.noreply.github.com>
2024-08-19 11:05:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Student-List
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Edberaga/Student-List)
---
+++ script.js
@@ -0,0 +1,18 @@
+const studentList = [];
+
+function addStudent() {
+  let name = prompt("Enter student name: ");
+  let score = prompt("Enter student score: ");
+  let className = prompt("Enter student class: ");
+
+  const newStudent = new Array(name, score, className);
+  studentList.push(newStudent);
+}
+
+const viewStudents = () => {
+  const str = "List of students: \n"
+  for(student of studentList) {
+    str += `${student[0]} from class ${student[2]} : ${student[1]}\n`;
+  }
+  alert(str);
+} 
